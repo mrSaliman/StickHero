cd091fc8a7bdf93d67b2a68e212e3d65fb0cb56c
mrSaliman <sloi.25abc@gmail.com>
2024-12-10 21:38:32 +0300

    init

---
+++ assets/Scripts/Libs/ObjectPool/ObjectPool.ts
@@ -0,0 +1,33 @@
+export class ObjectPool<T extends IPoolable> {
+    private pool: T[] = [];
+    private factory: () => T;
+
+    constructor(factory: () => T, size: number = 0) {
+        this.factory = factory;
+        this.expandPool(size)
+    }
+
+    get(): T {
+        if (this.pool.length > 0) {
+            const obj = this.pool.pop();
+            return obj;
+        }
+        const newObj = this.factory();
+        return newObj;
+    }
+
+    release(obj: T): void {
+        obj.reset();
+        this.pool.push(obj);
+    }
+
+    size(): number {
+        return this.pool.length;
+    }
+
+    expandPool(size: number): void {
+        for (let i = 0; i < size; i++) {
+            this.pool.push(this.factory());
+        }
+    }
+}
